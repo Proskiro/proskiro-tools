ec2feb452738299b348f6f266e6da20d0f52b23f
esa-ikram <eikram@costar.com>
2026-02-19 11:11:42 +0000

    properly ignore certain files

--- build/lib/proskiro_tools/data/__init__.py
+++
@@ -1,6 +0,0 @@
-from proskiro_tools.data.profession import search_profession, rows_to_profession
-
-__all__ = [
-    "search_profession",
-    "rows_to_profession",
-]
--- build/lib/proskiro_tools/models/skills.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class Skill(BaseModel):
-    uri: str
-    preferred_title: str
